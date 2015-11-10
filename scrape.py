dbea37605004ab6d59bbb7fe11641a338465870f
Jeff Stern <jeffstern.ny@gmail.com>
2015-11-09 20:11:02 -0500

    Converted datetime correctly

--- scrape.py
+++ scrape.py
@@ -10,6 +10,9 @@ import re
 import csv
 import sys
 import datetime
+import time
+from dateutil import parser
+from datetime import date, timedelta
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -69,44 +72,22 @@ def download_photo(img_url, filename):
 		return False
 	return True
 
-def create_csv_with_metadata():
-	output_file = open("collecteddata_"+get_current_date_string()+".csv", 'wb')
-	output = csv.writer(output_file)
-	output.writerow(["URL", "Text", "Links", "Image URLS", "Image Alt Text","Tags"])
-
-	for line in open("urllist.txt", 'r'):	
-		try:
-			site = parse_html(line)
-			copy = get_copy(site)
-			copyText = ""
-			copyLinksHref = []
-			if copy:
-				copyText = convert_to_plain_text(copy)
-				copyLinksHref = get_all_attr(copy.find_all("a"), "href")
-			tags = get_tags(site)
-			images = get_images(site)
-			if images:
-				imagesSrc = get_all_attr(images, "src")
-				imagesAlt = get_all_attr(images, "alt")
-
-			data = [line, copyText, copyLinksHref, imagesSrc, imagesAlt,tags]
-			output.writerow([unicode(s).encode("utf-8") for s in data])
-
-			print(line)
-		except:
-			print("Failed - likely because of special character in URL")
-			print(line)
-			sys.exit(0)
-
-	output_file.close()
+def convert_to_datetime(timestampString):
+	today = date.today()
+	yesterday = date.today() - timedelta(1)
+	timestampString = timestampString.replace("Today", today.strftime('%b %d, %Y')).replace("Yesterday", yesterday.strftime('%b %d, %Y'))
+	timestampString = timestampString.replace("March","Mar").replace("Sept","Sep").replace("June","Jun").replace("July","Jul").replace("April","Apr")
+	datetime = parser.parse(timestampString)
+	return datetime
 
 
 ## CREATING THREADS TABLE
-'''
 f = open("threads.csv","wb")
 output = csv.writer(f)
 
-for i in range(1,226):
+site = parse_html("https://scratch.mit.edu/discuss/10/?page=1")
+totalPages = int(site.find_all("a","page")[-1].text)
+for i in range(1,totalPages+1):
 	site = parse_html("https://scratch.mit.edu/discuss/10/?page="+str(i))
 	threads = site.find_all("tr")
 	currentIter = 0
@@ -117,7 +98,7 @@ for i in range(1,226):
 		replies = site.find_all("td","tc2")[currentIter].text
 		views = site.find_all("td","tc3")[currentIter].text
 		lr =  site.find_all("td","tcr")[currentIter]
-		lastresponse = lr.find("a").text
+		lastresponse = convert_to_datetime(lr.find("a").text)
 		lastresponder = lr.find("span","byuser").text[3:]
 		data = [link,title,user,replies,views,lastresponse,lastresponder]
 		output.writerow([unicode(s).encode("utf-8") for s in data])
@@ -125,7 +106,7 @@ for i in range(1,226):
 	print(i)
 
 f.close()
-'''
+
 
 ## CREATING POSTS TABLE
 
@@ -137,23 +118,25 @@ with open('threads.csv', 'rb') as f:
 
 f = open("posts.csv", "wb")
 output = csv.writer(f)
-for thread in threads:
+for thread in threads[i:i+endRow]:
 	threadURL = "https://scratch.mit.edu" + thread + "?page="
 	pageNumber = 1
 	totalPages = 1
+	site = parse_html("https://scratch.mit.edu" + thread + "?page=1")
+	pageList = site.find_all("a","page")
+	if (len(pageList)>0):
+		totalPages = int(site.find_all("a","page")[-1].text)
 	while pageNumber <= totalPages:
-		site = parse_html("https://scratch.mit.edu" + thread + "?page="+ str(pageNumber))
-		if pageNumber == 1:
-			pageList = site.find_all("a","page")
-			if (len(pageList)>0):
-				totalPages = site.find_all("a","page")[-1].text
+		if pageNumber != 1:
+			site = parse_html("https://scratch.mit.edu" + thread + "?page="+ str(pageNumber))
+
 		posts = site.find_all("div","firstpost")
 		
 		for post in posts:
-			print(post)
-			postID = post["id"]		# Get Post ID
-			postNumber = post.find("span","conr").text # Post Number
+			postID = int(post["id"][1:])		# Get Post ID
+			postNumber = int(post.find("span","conr").text.replace("#","")) # Post Number
  			timestamp =  post.find("div","box-head").find("a").text # Timestamp
+ 			timestamp = convert_to_datetime(timestamp)
  			content = post.find("div","post_body_html")
  			contentNoHTML = content.text # Post Content - No HTML
  			contentHTML = str(content)[28:-6] # Post Content - HTML
@@ -161,13 +144,14 @@ for thread in threads:
  			postcount = re.sub("[\t\n ]","",post.find('div','postleft').text.replace("Scratcher","").replace(user,""))  # User Post Count
 			editedMessage = post.find("em","posteditmessage")
 			edited = True if editedMessage else False # Was Edited?
-			editDate = editedMessage.text[editedMessage.text.rfind("(")+1:-1] if edited else "" # Timestamp of edit
+			editDate = convert_to_datetime(editedMessage.text[editedMessage.text.rfind("(")+1:-1]) if edited else "" # Timestamp of edit
  			# Post Signature
  			signature = post.find("div","postsignature")
  			signatureNoHTML =  signature.text if signature else ""
  			signatureHTML = str(signature)[27:-6] if signature else ""
 			data = [thread, postID, postNumber, timestamp, contentNoHTML, contentHTML, user, postcount, edited, editDate, signatureNoHTML, signatureHTML]
 			output.writerow([unicode(s).encode("utf-8") for s in data])
+			print(thread + " " + str(postNumber))
 		pageNumber += 1
 
 f.close()
