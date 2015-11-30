1f7d7aad28a55a79cf7bfabe3f4f30ee75ca6a13
Jeff Stern <jeffstern.ny@gmail.com>
2015-11-30 18:21:05 -0500

    Finished scraping, beginning analysis

--- scrape.py
+++ scrape.py
@@ -1,5 +1,5 @@
-# Jeff Stern, dedicated to Cait Holman
-# Scrapes http://apod.nasa.gov/apod/ and downloads all photos
+# Jeff Stern
+# Scrapes Scratch Collaboration forum
 
 from bs4 import BeautifulSoup
 from urllib import FancyURLopener
@@ -11,17 +11,27 @@ import csv
 import sys
 import datetime
 import time
+import getpass
 from dateutil import parser
 from datetime import date, timedelta
 
+csv.field_size_limit(sys.maxsize)
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def parse_html(url):
+def parse_html(url, local=False):
 	req = urllib2.Request(url)
 	response = urllib2.urlopen(req)
 	content = response.read()
 	return BeautifulSoup(content, "html.parser")
+
+def download_to_html(pageURL, filename=False):
+	page = urllib2.urlopen(pageURL)
+	page_content = page.read()
+	if filename == False:
+		filename = pageURL[pageURL.rfind('/')+1:]+'.html' 
+	with open(filename, 'w') as fid:
+		fid.write(page_content)
 
 def get_current_date_string():
 	return datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
@@ -82,76 +92,148 @@ def convert_to_datetime(timestampString):
 
 
 ## CREATING THREADS TABLE
-f = open("threads.csv","wb")
-output = csv.writer(f)
+def createThreadsTable():
+	f = open("threads.csv","wb")
+	output = csv.writer(f)
 
-site = parse_html("https://scratch.mit.edu/discuss/10/?page=1")
-totalPages = int(site.find_all("a","page")[-1].text)
-for i in range(1,totalPages+1):
-	site = parse_html("https://scratch.mit.edu/discuss/10/?page="+str(i))
-	threads = site.find_all("tr")
-	currentIter = 0
-	for thread in threads[1:]:
-		link = thread.find("h3","topic_isread").find("a").attrs["href"]
-		title = thread.find("h3","topic_isread").find("a").text
-		user = thread.find("span","byuser").text[3:]
-		replies = site.find_all("td","tc2")[currentIter].text
-		views = site.find_all("td","tc3")[currentIter].text
-		lr =  site.find_all("td","tcr")[currentIter]
-		lastresponse = convert_to_datetime(lr.find("a").text)
-		lastresponder = lr.find("span","byuser").text[3:]
-		data = [link,title,user,replies,views,lastresponse,lastresponder]
-		output.writerow([unicode(s).encode("utf-8") for s in data])
-		currentIter += 1
-	print(i)
+	site = parse_html("https://scratch.mit.edu/discuss/10/?page=1")
+	totalPages = int(site.find_all("a","page")[-1].text)
+	for i in range(1,totalPages+1):
+		site = parse_html("https://scratch.mit.edu/discuss/10/?page="+str(i))
+		threads = site.find_all("tr")
+		currentIter = 0
+		for thread in threads[1:]:
+			link = thread.find("h3","topic_isread").find("a").attrs["href"]
+			title = thread.find("h3","topic_isread").find("a").text
+			user = thread.find("span","byuser").text[3:]
+			replies = site.find_all("td","tc2")[currentIter].text
+			views = site.find_all("td","tc3")[currentIter].text
+			lr =  site.find_all("td","tcr")[currentIter]
+			lastresponse = convert_to_datetime(lr.find("a").text)
+			lastresponder = lr.find("span","byuser").text[3:]
+			data = [link,title,user,replies,views,lastresponse,lastresponder]
+			output.writerow([unicode(s).encode("utf-8") for s in data])
+			currentIter += 1
+		print(i)
 
-f.close()
+	f.close()
 
 
 ## CREATING POSTS TABLE
+def createPostsTable():
+	threads = []
+	with open('threads.csv', 'rb') as f:
+	    reader = csv.reader(f)
+	    for row in reader:
+	    	threads.append(row[0])
 
-threads = []
-with open('threads.csv', 'rb') as f:
+	f = open("posts.csv", "wb")
+	output = csv.writer(f)
+	for thread in threads:
+		threadURL = "https://scratch.mit.edu" + thread + "?page="
+		pageNumber = 1
+		totalPages = 1
+		site = parse_html("https://scratch.mit.edu" + thread + "?page=1")
+		pageList = site.find_all("a","page")
+		if (len(pageList)>0):
+			totalPages = int(site.find_all("a","page")[-1].text)
+		while pageNumber <= totalPages:
+			if pageNumber != 1:
+				site = parse_html("https://scratch.mit.edu" + thread + "?page="+ str(pageNumber))
+
+			posts = site.find_all("div","firstpost")
+			
+			for post in posts:
+				postID = int(post["id"][1:])		# Get Post ID
+				postNumber = int(post.find("span","conr").text.replace("#","")) # Post Number
+	 			timestamp =  post.find("div","box-head").find("a").text # Timestamp
+	 			timestamp = convert_to_datetime(timestamp)
+	 			content = post.find("div","post_body_html")
+	 			contentNoHTML = content.text # Post Content - No HTML
+	 			contentHTML = str(content)[28:-6] # Post Content - HTML
+	 			user = post.find('a','username').text # User
+	 			postcount = re.sub("[\t\n ]","",post.find('div','postleft').text.replace("Scratcher","").replace(user,""))  # User Post Count
+				editedMessage = post.find("em","posteditmessage")
+				edited = True if editedMessage else False # Was Edited?
+				editDate = convert_to_datetime(editedMessage.text[editedMessage.text.rfind("(")+1:-1]) if edited else "" # Timestamp of edit
+	 			# Post Signature
+	 			signature = post.find("div","postsignature")
+	 			signatureNoHTML =  signature.text if signature else ""
+	 			signatureHTML = str(signature)[27:-6] if signature else ""
+				data = [thread, postID, postNumber, timestamp, contentNoHTML, contentHTML, user, postcount, edited, editDate, signatureNoHTML, signatureHTML]
+				output.writerow([unicode(s).encode("utf-8") for s in data])
+				print(thread + " " + str(postNumber))
+			pageNumber += 1
+	f.close()
+
+
+## CREATING POSTS TABLE
+users = []
+with open('posts.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-    	threads.append(row[0])
+    	user = row[6]
+    	if user not in users:
+	    	users.append(row[6])
+'''
+# Save sample users for local testing
+for user in users[:10]:
+	try:
+		download_to_html("http://scratch.mit.edu/users/"+user)
+	except:
+		continue
+'''
 
-f = open("posts.csv", "wb")
+def getFollowCount(site):
+	followingHeader = site.find("h2").text
+	lLoc = followingHeader.rfind("(")+1
+	rLoc = followingHeader.rfind(")")
+	return followingHeader[lLoc:rLoc]
+
+localTestUsers = ['Hamish752', 'Novakitty', 'Xeno-', '13114DartKayl', 'GoldPing', 'pillow123456789', 'tm-z1', 'Mathstr0', 'TheSockMaster']
+
+f = open("users.csv", "wb")
 output = csv.writer(f)
-for thread in threads[i:i+endRow]:
-	threadURL = "https://scratch.mit.edu" + thread + "?page="
-	pageNumber = 1
-	totalPages = 1
-	site = parse_html("https://scratch.mit.edu" + thread + "?page=1")
-	pageList = site.find_all("a","page")
-	if (len(pageList)>0):
-		totalPages = int(site.find_all("a","page")[-1].text)
-	while pageNumber <= totalPages:
-		if pageNumber != 1:
-			site = parse_html("https://scratch.mit.edu" + thread + "?page="+ str(pageNumber))
-
-		posts = site.find_all("div","firstpost")
-		
-		for post in posts:
-			postID = int(post["id"][1:])		# Get Post ID
-			postNumber = int(post.find("span","conr").text.replace("#","")) # Post Number
- 			timestamp =  post.find("div","box-head").find("a").text # Timestamp
- 			timestamp = convert_to_datetime(timestamp)
- 			content = post.find("div","post_body_html")
- 			contentNoHTML = content.text # Post Content - No HTML
- 			contentHTML = str(content)[28:-6] # Post Content - HTML
- 			user = post.find('a','username').text # User
- 			postcount = re.sub("[\t\n ]","",post.find('div','postleft').text.replace("Scratcher","").replace(user,""))  # User Post Count
-			editedMessage = post.find("em","posteditmessage")
-			edited = True if editedMessage else False # Was Edited?
-			editDate = convert_to_datetime(editedMessage.text[editedMessage.text.rfind("(")+1:-1]) if edited else "" # Timestamp of edit
- 			# Post Signature
- 			signature = post.find("div","postsignature")
- 			signatureNoHTML =  signature.text if signature else ""
- 			signatureHTML = str(signature)[27:-6] if signature else ""
-			data = [thread, postID, postNumber, timestamp, contentNoHTML, contentHTML, user, postcount, edited, editDate, signatureNoHTML, signatureHTML]
-			output.writerow([unicode(s).encode("utf-8") for s in data])
-			print(thread + " " + str(postNumber))
-		pageNumber += 1
-
+i = 0
+for user in users[i:]:
+	#site = parse_html("file:users/" + user + ".html")
+	#followingSite = parse_html("file:users/"+user+"_following.html")
+	#followerSite = parse_html("file:users/"+user+"_followers.html")
+	site = ""	
+	try:
+		site = parse_html("https://scratch.mit.edu/users/"+user)
+	except:
+		data = [user, 0, 0, 0, 0, 0, 0, "", "", "Profile unavailable", "", "", ""]
+		output.writerow([unicode(s).encode("utf-8") for s in data])
+		print (user + " - Unavailable - " + str(i))
+		i += 1
+		continue
+	followingSite = parse_html("https://scratch.mit.edu/users/"+user+"/following/")
+	followerSite = parse_html("https://scratch.mit.edu/users/"+user+"/followers/")
+	bio = site.find(id="bio-readonly").text
+	joinDate = site.find("p","profile-details").find_all("span")[1]['title']
+	joinDate = convert_to_datetime(joinDate)
+	location = site.find("span","location")
+	status = site.find(id="status-readonly").text
+	counts =  { 'Shared Projects': 0, 'Favorite Projects': 0, "Studios I'm Following": 0, "Studios I Curate": 0, 'Following': 0, 'Followers': 0}
+	for title in site.find_all("h4")[:4]:
+		headerSplit = title.text[:-1].split(" (")
+		if headerSplit[0] in counts:
+			counts[headerSplit[0]] = int(headerSplit[1])
+	counts["Following"] = getFollowCount(followingSite)
+	counts["Followers"] = getFollowCount(followerSite)
+	featuredProjectName, featuredProjectURL = "", ""
+	featured = site.find("a","project-name")
+	if featured:
+		featuredProjectName = featured.text
+		featuredProjectURL = featured['href']
+	data = [user, counts['Shared Projects'], counts['Favorite Projects'], counts["Studios I'm Following"], counts["Studios I Curate"], counts["Following"], counts["Followers"], joinDate, location, bio, status, featuredProjectName, featuredProjectURL]
+	output.writerow([unicode(s).encode("utf-8") for s in data])
+	print(user + " " + str(i))
+	i += 1
 f.close()
+#	download_to_html("http://scratch.mit.edu/users/"+user+"/following", user+"_following.html")
+#	download_to_html("http://scratch.mit.edu/users/"+user+"/followers", user+"_followers.html")
+	
+
+# To get follower count and following count, have to go "/followers" and "/following"
